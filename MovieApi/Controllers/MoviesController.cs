8a4d9615c826a7d03c7351d07fb68bf313f779be
Bayley <cbayley@proforma.com>
2018-04-19 10:01:46 -0400

    Removed unneeded json/request parsing

--- MovieApi/Controllers/MoviesController.cs
+++ MovieApi/Controllers/MoviesController.cs
@@ -31,11 +31,6 @@ namespace MovieApi.Controllers
             }
         }
 
-        //public IEnumerable<Movie> Get(string title)
-        //{
-        //    return Get(title, "");
-        //}
-
         // GET api/movies?title=title&genre=genre
         public IEnumerable<Movie> Get(string title, string genre)
         {
@@ -60,10 +55,7 @@ namespace MovieApi.Controllers
             var movie = new Movie
             {
                 Title = title,
-                //YearReleased = yearReleased,
                 Genre = genre,
-                //Tagline = tagline,
-                //Rating = rating,
             };
             using (var db = new MoviesDB())
             {
@@ -74,10 +66,8 @@ namespace MovieApi.Controllers
         }
 
         // POST api/movies
-        public async Task<Movie> Post(HttpRequestMessage request)
+        public Movie Post(Movie movie)
         {
-            var jsonString = await request.Content.ReadAsStringAsync();
-            var movie = JsonConvert.DeserializeObject<Movie>(jsonString);
             using (var db = new MoviesDB())
             {
                 db.Movies.Add(movie);
